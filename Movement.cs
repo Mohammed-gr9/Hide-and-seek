42bf105cca53fc61dbb3d012ae49cd1d9782243e
Mohammed-gr9 <Mgr5889@gmail.com>
2026-02-25 11:41:40 +0300

    The player's basic movement
    It literally makes the player move

---
+++ Movement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 5f;
+    private Vector2 input;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Flip();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        input = value.Get<Vector2>();
+    }
+
+    void Flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }  
+    
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + input * speed * Time.fixedDeltaTime);
+    }
+
+
+
+}
